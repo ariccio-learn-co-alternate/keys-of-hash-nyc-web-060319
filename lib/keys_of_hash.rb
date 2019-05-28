736a8af722542d16789b1cded80fcefe427e2941
Alexander Riccio <test35965@gmail.com>
2019-05-28 22:22:13 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,7 +2,8 @@ class Hash
   def keys_of(*arguments)
     keys_out = []
     arguments.each do |arg|
-      matching = self.select{
+      matching = self.select{|k,v| v == arg}
+      
     end
     
     
